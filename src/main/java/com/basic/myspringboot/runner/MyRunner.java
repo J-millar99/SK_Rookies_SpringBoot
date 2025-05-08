9f78290bfa32ffe9b1624143c0d9fa8ec635623c
J-millar99 <wlwogus33@gmail.com>
2025-05-08 14:52:37 +0900

    MyRunner update

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -8,7 +8,18 @@ import org.springframework.stereotype.Component;
 public class MyRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        // foo라는 VM 아큐먼트가 있는지 확인
         System.out.println("VM argument foo : " + args.containsOption("foo"));
+        // bar 라는 Program 아규먼트가 있는지 확인
         System.out.println("Program argument bar : " + args.containsOption("bar"));
+
+        /*
+            Iterable forEach(Consumer)
+            Consumer는 함수형 인터페이스 vodi accept(T t)
+            Consumer의 추상 메서드를 오버라이딩하는 구문을 람다식으로 작성해보자
+         */
+        // 아규먼트 목록 출력
+        args.getOptionNames() // set<String>
+                .forEach(name -> System.out.println(name));
     }
 }
