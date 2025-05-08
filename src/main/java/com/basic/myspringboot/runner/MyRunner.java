1c7ef4b9741bcaeaef1cd649fe36245a50757385
J-millar99 <wlwogus33@gmail.com>
2025-05-08 18:18:20 +0900

    props add

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,13 +1,22 @@
 package com.basic.myspringboot.runner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements ApplicationRunner {
+
+    @Value("${myboot.name}")
+    private String name;
+
+    @Value("${myboot.age}")
+    private int age;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
         // foo라는 VM 아큐먼트가 있는지 확인
         System.out.println("VM argument foo : " + args.containsOption("foo"));
         // bar 라는 Program 아규먼트가 있는지 확인
