a2f7addfb2e739209fba41d294a6fef0b00afe57
J-millar99 <wlwogus33@gmail.com>
2025-05-09 01:44:48 +0900

    dynamic update

--- src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -16,15 +16,52 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+
 //@DataJpaTest
 class CustomerRepositoryTest {
     @Autowired
     CustomerRepository customerRepository;
 
     @Test
+    @Rollback(value = false)
+    void testUpdateCustomer() {
+        Customer customer = customerRepository.findById(1L)
+                .orElseThrow(() -> new RuntimeException("Customer Not Found"));
+        // 수정하려면 Entity의 setter method를 호출한다. dirty read
+        // update customers set customer_id=?,customer_name=? where id=? (@DynamicUpdate 적용 전)
+        // update customers set customer_name=? where id=? (@DynamicUpdate 적용 후)
+        customer.setCustomerName("SpringBoot");
+//        customerRepository.save(customer);
+        assertThat(customer.getCustomerName()).isEqualTo("SpringBoot");
+    }
+
+    @Test
+    void testByNotFoundException() {
+        // <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier)
+        // Supplier의 추상메서드 T get()
+        Customer customer = customerRepository.findByCustomerId("A004").orElseThrow(() -> new RuntimeException("Customer Not Found"));
+        // assertThat(customer.getCustomerId()).isEqualTo("A001");
+    }
+
+    @Test
+    @Disabled
     void testFindBy() {
         Optional<Customer> optionalCustomer = customerRepository.findById(1L);
-        assertThat(optionalCustomer).isNotEmpty();
+        if (optionalCustomer.isPresent()) {
+            Customer existCustomer = optionalCustomer.get();
+            assertThat(existCustomer.getId()).isEqualTo(1L);
+        }
+
+        // Optional의 T orElseGet(Supplier<? extends T> supplier)
+        // Supplier의 추상메서드 T get()
+        // 고객번호가 존재하는 경우
+        Optional<Customer> optionalCustomer2 = customerRepository.findByCustomerId("A001");
+        Customer a001customer = optionalCustomer.orElseGet(() -> new Customer());
+        assertThat(a001customer.getCustomerName()).isEqualTo("스프링");
+        // 고객번호가 존재하지 않는 경우
+        Customer notFoundCustomer =
+                customerRepository.findByCustomerId("A004").orElseGet(() -> new Customer());
+        assertThat(notFoundCustomer.getCustomerName()).isNull();
     }
 
     @Test
@@ -34,12 +71,12 @@ class CustomerRepositoryTest {
         // Given
         Customer customer = new Customer();
         customer.setCustomerId("A002");
-        customer.setCustomerName("스프링");
+        customer.setCustomerName("스프링2");
         // When
         Customer addCustomer = customerRepository.save(customer);
         // Then
         assertThat(addCustomer).isNotNull();
-        assertThat(addCustomer.getCustomerName()).isEqualTo("스프링");
+        assertThat(addCustomer.getCustomerName()).isEqualTo("스프링2");
 
     }
 
