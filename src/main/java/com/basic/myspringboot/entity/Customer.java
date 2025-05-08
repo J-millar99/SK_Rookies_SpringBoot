a3a94805112d320196c8995a2aadde55d7b41d00
J-millar99 <wlwogus33@gmail.com>
2025-05-09 00:15:34 +0900

    DatabaseRunner update

---
+++ src/main/java/com/basic/myspringboot/entity/Customer.java
@@ -0,0 +1,21 @@
+package com.basic.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="customers")
+@Getter @Setter
+public class Customer {
+    // Primary Key, pk 값을 persistence provider 가 결정해라
+    @Id @GeneratedValue(strategy =  GenerationType.AUTO)
+    private Long id;
+
+    // 중복을 허용하지 않고, Null 값을 허용하지 않음
+    @Column(unique = true, nullable = false)
+    private String customerId;
+    // Null 값을 허용하지 않음
+    @Column(nullable = false)
+    private String customerName;
+}
