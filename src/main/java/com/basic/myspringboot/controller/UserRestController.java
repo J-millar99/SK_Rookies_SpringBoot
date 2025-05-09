0647e979232d17f8a30e85fb585584bc29165168
J-millar99 <wlwogus33@gmail.com>
2025-05-10 00:04:53 +0900

    UserRepository UserRestController add

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -1,17 +1,22 @@
 package com.basic.myspringboot.controller;
 
+import com.basic.myspringboot.entity.Customer;
+import com.basic.myspringboot.repository.CustomerRepository;
 import com.basic.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor // final인 변수를 초기화하는 생성자를 자동으로 생성해주는 역할을 하는 롬복 어노테이션
+@RequestMapping("/api/users")
 public class UserRestController {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     // Constructor Injection
-    public UserRestController(UserRepository userRepository) {
-        System.out.println(">>> UserController " + userRepository.getClass().getName());
-        this.userRepository = userRepository;
-    }
-
+//    public UserRestController(UserRepository userRepository) {
+//        System.out.println(">>> UserController " + userRepository.getClass().getName());
+//        this.userRepository = userRepository;
+//    }
 
 }
